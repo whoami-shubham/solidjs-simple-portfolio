68790f6af92a114d9e8d8e4cbf66f20f87401387
shubham jha <shubhamjha@sharechat.co>
2022-09-01 02:16:44 +0530

    fix responsiveness

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 function Card({ title, url, description }: CardProps) {
   return (
-    <div class="bg-cardbg flex flex-col border-2 border-cardborder py-4 px-8 rounded-2xl my-4 w-[360px] mr-4">
+    <div class="bg-cardbg flex flex-col border-2 border-cardborder py-4 px-8 rounded-2xl my-4 lg:w-[360px] md:w-[360px] mr-4">
       <div class="flex justify-between">
         <span class="text-[16px] text-skyblue font-[600]">{title}</span>
         <a href={url}>
@@ -24,7 +24,7 @@ function Cards({ data }: { data: { title: string; data: CardProps[] } }) {
   return (
     <>
       <h2 class="text-white text-[30px] pb-6">{data?.title}</h2>
-      <div class="flex flex-wrap">
+      <div class="flex flex-wrap sm:flex-col">
         <For each={data?.data} fallback={null}>
           {(item, index) => <Card {...item} />}
         </For>
