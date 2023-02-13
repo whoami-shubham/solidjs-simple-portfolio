0b0b78e12b2d379ed74421ec8ab877fea4254bd2
shubham jha <shubhamkumarjha0013@gmail.com>
2023-02-13 23:49:23 +0530

    fix: replace static keyword

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import solid from "solid-start/vite";
-import static from "solid-start-static";
+import solidStatic from "solid-start-static";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/solidjs-simple-portfolio/",
   // insert your github project name between slashes above
-  plugins: [solid({ adapter: static() })],
+  plugins: [solid({ adapter: solidStatic() })],
 });
